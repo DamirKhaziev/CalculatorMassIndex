23ee7838f6282f405f55e1bad451598576d2507d
KhazievDR <haziev1999@list.ru>
2021-10-05 11:52:48 +0300

    DOM addEventListener

---
+++ app.js
@@ -0,0 +1,12 @@
+// const buttonEl = document.getElementById('button')
+// console.log (buttonEl)
+//
+// // buttonEl.addEventListener('click', (event) => {
+//     console.log(event)
+// })
+//
+// buttonEl.setAttribute('id', 'newButton')
+//
+// console.log(buttonEl.id)
+
+const inputEl = document.getElementById('input')

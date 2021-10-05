c6f30b6277a917e02fadbe976ba075469e995314
KhazievDR <haziev1999@list.ru>
2021-10-05 12:20:08 +0300

    Индекс массы тела

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-// const buttonEl = document.getElementById('button')
-// console.log (buttonEl)
-//
-// // buttonEl.addEventListener('click', (event) => {
-//     console.log(event)
-// })
-//
-// buttonEl.setAttribute('id', 'newButton')
-//
-// console.log(buttonEl.id)
+import {calculate} from "./calculate.js";
 
-const inputEl = document.getElementById('input')
+const calculateBattonEl = document.getElementById("calculate")
+const inputWeightEl = document.getElementById("weight")
+const inputHeightEl = document.getElementById("height")
+const resultEl = document.getElementById("result")
+
+calculateBattonEl.addEventListener('click', () =>{
+    const weight = inputWeightEl.value
+    const height = inputHeightEl.value
+
+     const result = calculate(weight, height)
+
+    resultEl.textContent = result
+})
